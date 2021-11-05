37df0c6c9e58b701a5134f7be9f6c7b4e17a23f2
enrique <nohunenrique@gmail.com>
2021-11-04 20:18:47 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba Imagenes BWE</title>
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body>
+    
+    <section class="mt-100">
+        <h1>Hola pruebsa</h1>
+    <div class="container">
+    <div class="flex flex-wrap overflow-hidden">
+        
+ 
+        <div class="w-full overflow-hidden lg:w-1/3 xl:w-1/3">
+          <!-- Column Content -->
+          <img src="/img/dev.png" alt="">
+        </div>
+        
+        
+      </div>
+    </div>
+    </section>
+</body>
+</html>
